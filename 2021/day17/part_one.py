ceab5d2da7af7d11a540ca4ea5440f61de5a9ba5
Steven Terrana <steventerrana@gmail.com>
2021-12-19 16:58:30 -0500

    day17

--- 2021/day17/part_one.py
+++ 2021/day17/part_one.py
@@ -7,6 +7,7 @@ def parse_input():
   match = re.findall(r"-?\d+", input)
   return [ int(n) for n in match ]
 
+# get all valid initial velocities for a given y position
 def solve(y):
   n = sympy.Symbol('n', integer=True, positive=True)
   v = sympy.Symbol('v', integer=True)
@@ -17,9 +18,15 @@ def solve(y):
 
 (x_min, x_max, y_min, y_max) = parse_input()
 
+# get all valid initial velocities for each 
+# y coordinate in the target range.
 solutions = []
 for y in range(y_min, y_max+1): solutions.extend(solve(y))
 
+# Given the acceleration for y is constant at -1 we can
+# determine the max height as a finite sum from 0 to the
+# initial velocity (1 + 2 + 3 ... + v_max). Therefore, the
+# max height is (1/2)*(v_max * (v_max + 1)) 
 v_max = max([pair[1] for pair in solutions])
 h_max = int((v_max * (v_max + 1))/2)
 print(h_max)
--- 2021/day17/part_two.py
+++ 2021/day17/part_two.py
@@ -1,47 +1,87 @@
 import re
 import sympy
 from sympy.solvers.diophantine.diophantine import diophantine
-import itertools
 
 def parse_input():
-  input = open("2021/day17/test.txt","r").read().strip()
+  input = open("2021/day17/input.txt","r").read().strip()
   match = re.findall(r"-?\d+", input)
   return [ int(n) for n in match ]
 
-# def get_velocities():
-#   input = open("2021/day17/velocities.txt", "r").read()
-#   match = re.findall(r"-?\d+,-?\d+", input)
-#   velocities = []
-#   for m in match:
-#     velocities.append(tuple([int(n) for n in m.split(",")]))
-#   return velocities
+# determines if a probe fired with an initial velocity (iv)
+# will hit a target area defined by x_range and y_range 
+def is_valid(iv, x_range, y_range):
+  (v_x, v_y) = iv
+  (x_min, x_max) = x_range
+  (y_min, y_max) = y_range
 
-def solve(d):
-  n = sympy.Symbol('n', integer=True, positive=True)
-  v = sympy.Symbol('v', integer=True)
-  eq = n * v - (n**2 - n)/2 - d
-  t, _ = sympy.symbols('t,t_0', integer=True)
-  solution = diophantine(eq, t, [n,v])
-  return solution
+  x = 0
+  y = 0
+  while True:
+    # increment location
+    prev_x = x
+    x += v_x
+    y += v_y
+    # increment velocity
+    v_x -= 1
+    if v_x < 0: v_x = 0
+    v_y -= 1
+    # is valid?
+    if x_min <= x <= x_max and y_min <= y <= y_max:
+      return True
+    # is miss? 
+    ## x-axis check: not moving and outside range
+    if prev_x == x and any([x < x_min, x > x_max]):
+      return False
+    ## y-axis check: y < y_min
+    if y < y_min:
+      return False
+
+# use sympy to solve for the lower and upper bounds
+# of velocity for a given range. 
+# 
+# We could have used sympy to just get all the initial 
+# velocites instead of just the upper/lower bounds.. 
+# but.. i don't think sympy supports 
+# systems of piecewise diophantine inequalities.
+#
+# system of equations:
+#               equation        | when
+#         n * v_x - (n^2 - n)/2 | n < v_x 
+# 1. x = /
+#        \
+#         (v_x * (v_x + 1))/2   | n >= v_x 
+# 2. y= n * v_y - (n^2 - n)/2
+#
+# constraints:
+# 1. n > 0
+# 2. x_min <= x <= x_max
+# 3. y_min <= y <= y_max
+# 4. all variables are integers
+def get_bounds(_min, _max):
+  solutions = []
+  for d in range(_min, _max+1):
+    n = sympy.Symbol('a', integer=True, positive=True)
+    v = sympy.Symbol('v', integer=True)
+    eq = n * v - (n**2 - n)/2 - d
+    t, _ = sympy.symbols('t,t_0', integer=True)
+    solutions.extend(diophantine(eq, t, [n,v]))
+  velocities = [ v[1] for v in solutions ]
+  upper = max(velocities)
+  lower = min(velocities)
+  return (lower, upper)
 
 (x_min, x_max, y_min, y_max) = parse_input()
-y_solutions = []
-for y in range(y_min, y_max+1): y_solutions.extend(solve(y))
-x_solutions = []
-for x in range(x_min, x_max+1): x_solutions.extend(solve(x))
+(yv_lower, yv_upper) = get_bounds(y_min, y_max)
+(xv_lower, xv_upper) = get_bounds(x_min, x_max)
 
-print(f'x_solutions = {x_solutions}')
-print((16,7) in x_solutions)
+# "intelligent" brute force checking each possible
+# initial velocity based on the upper/lower bounds
+# determined by sympy
+solution = []
+for yv in range(yv_lower, yv_upper+1):
+  for xv in range(xv_lower, xv_upper+1):
+    iv = (xv,yv)
+    if is_valid(iv, (x_min, x_max), (y_min,y_max)):
+      solution.append(iv)
 
-x_n = set([ pair[0] for pair in x_solutions ])
-y_n = set([ pair[0] for pair in y_solutions ])
-common = x_n.intersection(y_n)
-
-velocities = set()
-for n in common:
-  valid_x = [ v[1] for v in x_solutions if v[0] == n ]
-  valid_y = [ v[1] for v in y_solutions if v[0] == n ]
-  n_permutations = list(itertools.product(valid_x, valid_y))
-  velocities = velocities.union(n_permutations)
-
-print(len(velocities))
+print(len(set(solution)))
