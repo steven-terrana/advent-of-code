abef9ee31dd3e98574dd8ace2277e3e0c448a97a
Steven Terrana <steventerrana@gmail.com>
2023-12-01 08:16:31 -0500

    delete

--- 2020/day1/part_one.py
+++
@@ -1,7 +0,0 @@
-import itertools
-input = [ int(x) for x in open("2020/day1/input.txt", "r").readlines() ]
-
-for x in list(itertools.combinations(input, 2)):
-  if sum(x) == 2020:
-    print(x[0] * x[1])
-    break
