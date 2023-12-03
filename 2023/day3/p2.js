a03e986a21860a14828d1426a56280660a34b615
Steven Terrana <steventerrana@gmail.com>
2023-12-03 18:04:33 -0500

    using aabb's for day 2 part 1 and 2

--- 2023/day3/p2.js
+++ 2023/day3/p2.js
@@ -10,25 +10,27 @@ import chalk from 'chalk'
   have bounding boxes associated with them that we'll use to assess
   proximity (collision) with other... things.
   
-  symbols will have a margin of 0 for their bounding 
-  box whereas numbers will have a margin of 1. 
+  symbols will have a margin of 1 for their bounding 
+  box whereas numbers will have a margin of 0. 
 
-  for each number thing, we will iterate over the Things that are
-  a symbol and check if they intersect with the number's bounding box.
+  for each * symbol thing, we will iterate over the Things that are
+  a number and check if they intersect with a given * symbol's bounding 
+  box. If they do, we'll store it. 
 
-  If they do, we know this number is an engine part so we'll add it
-  to the running sum.
+  We can then check how many numbers have collided with the * symbol and
+  if it's two, multiple their values together and add it to the running sum.
 */
 
-const schematic = fs.readFileSync('input.txt', 'utf-8')
-const parser = new RegExp(/(?<number>\d+)|(?<symbol>[^\d\.\n])/gm)
 
-
+// there are going to be two types of Things... numbers and symbols
+// we are going to use this to filter Things later.
 const Types = Object.freeze({
 	number: Symbol("number"),
 	symbol: Symbol("symbol")
 })
 
+// an object represented on the Engine Schematic. We'll store the
+// type of thing it is, its value, and an AABB (hitbox..) for the thing
 class Thing {
   constructor(type, value, aabb){
     this.type = type
@@ -37,6 +39,8 @@ class Thing {
   }
 }
 
+// an axis-aligned bounding box (..rectangle in a 2D array)
+// see: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
 class AABB {
   constructor(min_y, max_y, min_x, max_x){
     this.min_y = min_y
@@ -55,7 +59,8 @@ class AABB {
 }
 
 // prints out two things a, b and displays their
-// bounding boxes in color
+// bounding boxes in color. This was used for
+// debugging.
 function prettyPrint(schematic, a, b){
   let y = 0
   for (const r of schematic.split('\n')){
@@ -74,35 +79,47 @@ function prettyPrint(schematic, a, b){
   }
 }
 
+// We're going to iterate over the engine schematic and construct 
+// all the.... Things
+const schematic = fs.readFileSync('input.txt', 'utf-8')
+const parser = new RegExp(/(?<number>\d+)|(?<symbol>[^\d\.\n])/gm)
+
 let things = []
 schematic.split('\n').forEach( (line, row) => {
   Array.from(line.matchAll(parser)).forEach( match => {
     let value = match[0]
     if (match.groups?.number) {
-      // for part 1: numbers have a margin of 1 for the bounding box
-      let aabb = new AABB(row - 1, row + 1, match.index - 1, match.index + value.length)
+      // for part 2: numbers have no margin for the bounding box
+      let aabb = new AABB(row, row, match.index, match.index + value.length-1)
       things.push(new Thing(Types.number, Number(value), aabb))
     }
     if (match.groups?.symbol) {
-      // for part 1: symbols have no margin for the bounding box
-      let aabb = new AABB(row, row, match.index, match.index)
+      // for part 2: symbols have a margin of 1 for the bounding box
+      let aabb = new AABB(row - 1, row + 1, match.index - 1, match.index + value.length)
       things.push(new Thing(Types.symbol, value, aabb))
     }
   })
 })
 
+// now let's check for which numbers intersect with the * symbols
+// and add them up (if the number of intersecting numbers is 2)
 let numbers = things.filter( t => t.type === Types.number )
-let symbols = things.filter( t => t.type === Types.symbol )
+let stars = things.filter( t => t.value === '*' )
 
 let sum = 0
-for (const n of numbers){
-  for (const s of symbols){
-    if(n.aabb.intersects(s.aabb)){
-      console.log('The following parts intersect', n, s)
-      sum += n.value
-      break
+for (const s of stars){
+  let collisions = []
+  for (const n of numbers){
+    // prettyPrint(schematic, n, s)
+    if (s.aabb.intersects(n.aabb)){
+      // console.log(chalk.bold.green('collision!'))
+      collisions.push(n)
     }
   }
+  if (collisions.length == 2){
+    sum += collisions[0].value * collisions[1].value
+  }
 }
-console.log("Part 1: ", sum)
+
+console.log("Part 2: ", sum)
 
