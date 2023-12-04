1a5eefe234e2444f303e68d4070b272aaa5b4bf6
Steven Terrana <steventerrana@gmail.com>
2023-12-04 12:55:07 -0500

    remove old folders

--- 2023/day2/p2-1-chain.js
+++
@@ -1,9 +0,0 @@
-console.log(
-  require('fs').readFileSync('input.txt', 'utf-8').split('\n').map( game => Object.values(Array.from(game.matchAll(new RegExp(/(?<red>\d+(?=\sred))|(?<green>\d+(?=\sgreen))|(?<blue>\d+(?=\sblue))/g))).reduce( (m, n) => {
-    return { 
-      red: n.groups.red ? Math.max(m.red, Number(n.groups.red)) : m.red, 
-      blue: n.groups.blue ? Math.max(m.blue, Number(n.groups.blue)) : m.blue, 
-      green: n.groups.green ? Math.max(m.green, Number(n.groups.green)) : m.green
-    } 
-  }, { red: 0, green: 0, blue: 0 })).reduce( (p, v) => p *= v, 1 )).reduce( (s, v) => s += v, 0)
-)
