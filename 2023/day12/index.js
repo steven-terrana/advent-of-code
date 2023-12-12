8932df055400c179c0a3579fd25790131af276c7
Steven Terrana <steventerrana@gmail.com>
2023-12-12 13:45:57 +0000

    in flight

--- 2023/day12/index.js
+++ 2023/day12/index.js
@@ -4,46 +4,36 @@ import ProgressBar from 'progress'
 class ConditionRecord{
   constructor(record, numbers){
     this.record = record
+    this.unknowns = [record.matchAll(/\?/g)].map(m => m.index)
+    this.knownSprings = [record.matchAll(/#/g)].length
     this.numbers = numbers
+    this.totalSprings = numbers.reduce( (s,n) => s+n,0)
   }
   permutate(){
-    let unsureIndices = [...this.record.matchAll(/\?/g)].map(m => m.index)
-    let totalSprings = this.numbers.reduce( (s,n) => s+n,0)
-    let nSprings = [...this.record.matchAll(/#/g)].length
     let arrangements = 0
     let attempted = []
     let replaceAt = (s, c, idx) => s.slice(0, idx) + c + s.slice(idx+1)
-    let permutatations = this.getCharacterSubstitutionPermutations(unsureIndices.length)
-    // console.log('permutations: ', permutatations.length)
-    for (let p of permutatations){
-      let pSprings = permutatations.filter(c => c=='#').length
-      if (nSprings + pSprings != totalSprings){
-        continue
-      }
-      let arrangement = unsureIndices.reduce( (arrangement, idx, i) => {
-        return replaceAt(arrangement, p[i], idx)
+    let permutations = getBooleanPermutations(this.unknowns.length)
+    permutations = permutations.filter(p => p.springs + this.knownSprings != this.totalSprings)
+    console.log(permutations)
+    for (let p of permutations){
+      // frontload with '.' characters. because this depends on how many
+      // unknowns there are, doing it earlier would have made getBooleanPermutations
+      // harder to memoize efficiently.
+      let substValues = [ Array(this.unknowns.length - p.value.length).fill('.'), ...p.value]
+      let arrangement = this.unknowns.reduce( (arrangement, idx, i) => {
+        return replaceAt(arrangement, substValues[i], idx)
       } , this.record)
       if(!attempted.includes(arrangement)){
+        console.log('attempting: ', arrangement)
         attempted.push(arrangement)
         if(this.validate(arrangement)){
+          console.log('--> valid')
           arrangements++
         }
       }
     }
     return arrangements
-  }
-  getCharacterSubstitutionPermutations(N){
-    let n = parseInt(Array(N).fill('1').join(''),2)
-    let permutations = []
-    for(let i = 0; i <= n; i++){
-      let b = (i >>> 0).toString(2)
-      if(b.length < N){
-        b = [...Array(N - b.length).fill('0'), b].join('') 
-      }
-      let p = b.split('').map( c => parseInt(c) ? '#' : '.')
-      permutations.push(p)
-    }
-    return permutations
   }
   // validates a given arrangement works or not
   validate(arrangement){
@@ -56,6 +46,29 @@ class ConditionRecord{
     numbers = numbers.split(',').map(n => parseInt(n))
     return new ConditionRecord(record, numbers)
   }
+}
+
+let cache = {}
+function getBooleanPermutations(N){
+  if (N == 1){
+    return [{value: ['#'], springs: 1}]
+  }
+  if (cache[N]){
+    return cache[N]
+  }
+  const chars = { '1': '#', '0': '.'}
+  let bin = (N >>> 0).toString(2)
+  let springs = 0
+  let bin2spring = bin.replace(/[10]/g, m => {
+    if (m == "#"){
+      springs++
+    }
+    return chars[m]
+  }).split('')
+
+  let p = [{value: bin2spring, springs: springs}, ...getBooleanPermutations(N-1)]
+  cache[N] = p
+  return p
 }
 
 function parseConditionRecords(file){
@@ -73,3 +86,4 @@ for (let r of records){
   bar.tick()
 }
 
+console.log(sum)
