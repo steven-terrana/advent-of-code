1dc3b94e362bd89a8832839e9a607f7be581c9b4
steven-terrana <steventerrana@gmail.com>
2024-12-02 01:24:08 -0500

    2024 02

---
+++ 2024/02/part1.py
@@ -0,0 +1,22 @@
+input = open('input.txt', 'r').read()
+
+reports = [ [int(n) for n in line.split()] for line in input.split('\n')]
+
+def isDirectional(report):
+    is_ascending = all([ report[i] < report[i + 1] for i in range(len(report) - 1) ])
+    is_descending = all([ report[i] > report[i + 1] for i in range(len(report) - 1) ])
+    return is_ascending or is_descending
+
+def isProximal(report):
+    proximities = [ abs(report[i] - report[i + 1]) for i in range(len(report) -1 ) ]
+    isSafe = all([ 1 <= p and p <= 3 for p in proximities])
+    if (isSafe): return True
+
+safe = 0
+for report in reports:
+    directional = isDirectional(report)
+    proximal = isProximal(report)
+    if directional and proximal:
+        safe = safe + 1
+
+print(safe)
---
+++ 2024/02/part2.py
@@ -0,0 +1,29 @@
+input = open('input.txt', 'r').read()
+
+reports = [ [int(n) for n in line.split()] for line in input.split('\n')]
+
+def isDirectional(report):
+    is_ascending = all([ report[i] < report[i + 1] for i in range(len(report) - 1) ])
+    is_descending = all([ report[i] > report[i + 1] for i in range(len(report) - 1) ])
+    return is_ascending or is_descending
+
+def isProximal(report):
+    proximities = [ abs(report[i] - report[i + 1]) for i in range(len(report) -1 ) ]
+    isSafe = all([ 1 <= p and p <= 3 for p in proximities])
+    if (isSafe): return True
+
+safe = 0
+for report in reports:
+    directional = isDirectional(report)
+    proximal = isProximal(report)
+    if directional and proximal:
+        safe = safe + 1
+    else:
+        for idx in range(len(report)):
+            newReport = report.copy()
+            del newReport[idx]
+            if isProximal(newReport) and isDirectional(newReport):
+                safe = safe + 1
+                break
+
+print(safe)
