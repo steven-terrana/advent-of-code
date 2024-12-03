fc2f38d30585ffc7c98fa37f7a9695a62a3a22df
steven-terrana <steventerrana@gmail.com>
2024-12-02 19:16:07 -0500

    style

--- 2024/02/part1.py
+++ 2024/02/part1.py
@@ -1,26 +1,25 @@
-input = open("input.txt", "r").read()
+with open("input.txt", "r") as file:
+    input = file.read()
 
-reports = [[int(n) for n in line.split()] for line in input.split("\n")]
+reports = [[int(level) for level in report.split()] for report in input.split("\n")]
 
 
-def isDirectional(report):
+def is_directional(report):
+    """Check if the report is strictly ascending or descending."""
     is_ascending = all([report[i] < report[i + 1] for i in range(len(report) - 1)])
     is_descending = all([report[i] > report[i + 1] for i in range(len(report) - 1)])
     return is_ascending or is_descending
 
 
-def isProximal(report):
+def is_proximal(report):
+    """Check if the absolute differences between consecutive elements are within [1, 3]."""
     proximities = [abs(report[i] - report[i + 1]) for i in range(len(report) - 1)]
-    isSafe = all([1 <= p and p <= 3 for p in proximities])
-    if isSafe:
-        return True
+    return all([1 <= p <= 3 for p in proximities])
 
 
 safe = 0
 for report in reports:
-    directional = isDirectional(report)
-    proximal = isProximal(report)
-    if directional and proximal:
-        safe = safe + 1
+    if is_directional(report) and is_proximal(report):
+        safe += 1
 
 print(safe)
